f51e5759002f87e21b52db7ddbf7b788fb96213f
ljcanales <ljcanales1404@gmail.com>
2021-09-01 18:53:21 -0300

    update

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const routes = require('./routes/index.js');
 
 // settings
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
 
 // routes
-app.use(routes);
+app.use("/", routes);
 
 app.listen(app.get('port'), () => {
     console.log(`Server running on port ${app.get('port')} ...`);
