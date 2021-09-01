41bca95461ecf1ebd5b7281ab7b28e21896862e1
ljcanales <ljcanales1404@gmail.com>
2021-09-01 19:26:08 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ router.get('/:linkName', (req, res) => {
     const { linkName } = req.params;
     return res.redirect(redirectLinks[linkName]);
 });
+router.get('/', (req, res) => {
+    return res.send("Hello World");
+});
 
 module.exports = router;
